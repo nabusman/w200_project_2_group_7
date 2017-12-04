973661e3ef3710344a53773abc13d5d8d2e4ffae
Nabeel Siddiqi <nabs@Nabeels-MBP.attlocal.net>
2017-12-03 20:00:24 -0800

    load data and save as h5

--- load_data.py
+++ load_data.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
+# Load data from stata
 df = pd.read_stata('GSS7216_R1a.dta', convert_categoricals = False)
+
+# Save data as HDFStore
+store = pd.HDFStore('store.h5')
+store['df'] = df
